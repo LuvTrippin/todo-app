5a7130bb24329644b1bc1132329b5ce1d82bf634
Timur Sergeev <82363203+LuvTrippin@users.noreply.github.com>
2024-01-08 18:30:21 +0500

    refactor: add anonimous function wrapper

--- app.js
+++ app.js
@@ -1,203 +1,206 @@
+(function () {
 
-const todos = [];
-const users = [];
-
-document.addEventListener('DOMContentLoaded', initApp);
-
-function getUserName(userId) {
-    const user = users.find(user => user.id === userId);
-    return user.name;
-}
-
-function createTodoElement({id, userId, title, completed}) {
-    const li = document.createElement('li');
-    li.className = "todo-item";
-    li.dataset.id = id;
-    li.innerHTML = `<span class="todo">${title} <i>by</i> <b>${getUserName(userId)}</b></span>`;
-
-    const status = document.createElement('input');
-    status.type = 'checkbox';
-    status.checked = completed;
-    status.addEventListener('change', handleStatusChange);
-
-    const close = document.createElement('span');
-    close.innerHTML = '&times';
-    close.className = 'close';
-    close.addEventListener('click', handleClose);
-
-    li.prepend(status);
-    li.append(close);
-
-    return li;
-}
-
-function printTodo(todo) {
-    const todoList = document.querySelector('#todo-list');
-    todoList.prepend(createTodoElement(todo));
-}
-
-function printTodos(todos) {
-    todos.forEach(todo => {
-        printTodo(todo);
-    });
-}
-
-function removeTodo(todoId) {
-    todos.filter(todo => todo.id !== todoId);
-
-    const todo = document.querySelector('ul').querySelector(`[data-id="${todoId}"]`);
-    todo.querySelector('input').removeEventListener('change', handleStatusChange);
-    todo.querySelector('.close').removeEventListener('click', handleClose);
-    todo.remove();
-}
-
-function alertError(error) {
-    alert(error.message);
-}
-
-function createUserOption(user) {
-    const op = document.createElement('option');
-    op.value = user.id;
-    op.innerText = user.name;
+    const todos = [];
+    const users = [];
     
-    return op;
-}
-
-function printUserOption(user) {
-    const select = document.querySelector('#user-todo');
-    select.append(createUserOption(user));
-}
-
-function printUsersOptions(users) {
-    users.forEach(user => {
-        printUserOption(user);
-    });
-}
-
-function handleSubmit(event) {
-    event.preventDefault();
-
-    createTodo({
-        userId: Number(this.user.value),
-        title: this.todo.value,
-        completed: false,
-    });
-
-    this.reset();
-}
-
-function handleStatusChange() {
-    changeTodoStatus(this.parentElement.dataset.id, this.checked);
-}
-
-function handleClose() {
-    deleteTodo(this.parentElement.dataset.id);
-}
-
-function initApp() {
-    Promise.all([getAllTodos(), getAllUsers()])
-    .then(values => {
-        todos.push(...values[0]);
-        users.push(...values[1]);
-
-        printUsersOptions(users);
-        printTodos(todos);
-    })
-
-    const form = document.querySelector('form');
-    form.addEventListener('submit', handleSubmit);
-}
-
-
-async function getAllTodos() {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-
-        if (!response.ok) {
-            throw new Error('Cannot fetch');
-        }
-        
-        const data = await response.json();
+    document.addEventListener('DOMContentLoaded', initApp);
     
-        return data   
-    } catch (error) {
-        alertError(error);
+    function getUserName(userId) {
+        const user = users.find(user => user.id === userId);
+        return user.name;
     }
-}
-
-async function getAllUsers() {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-
-        if (!response.ok) {
-            throw new Error('Cannot fetch');
-        }
-
-        const data = await response.json();
-
-        return data
-    } catch (error) {
-        alertError(error);
+    
+    function createTodoElement({id, userId, title, completed}) {
+        const li = document.createElement('li');
+        li.className = "todo-item";
+        li.dataset.id = id;
+        li.innerHTML = `<span class="todo">${title} <i>by</i> <b>${getUserName(userId)}</b></span>`;
+    
+        const status = document.createElement('input');
+        status.type = 'checkbox';
+        status.checked = completed;
+        status.addEventListener('change', handleStatusChange);
+    
+        const close = document.createElement('span');
+        close.innerHTML = '&times';
+        close.className = 'close';
+        close.addEventListener('click', handleClose);
+    
+        li.prepend(status);
+        li.append(close);
+    
+        return li;
     }
-}
-
-async function createTodo(todo) {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
-            method: 'POST',
-            body: JSON.stringify(todo),
-            headers: {
-                'Content-Type': 'application/json',
-            }
+    
+    function printTodo(todo) {
+        const todoList = document.querySelector('#todo-list');
+        todoList.prepend(createTodoElement(todo));
+    }
+    
+    function printTodos(todos) {
+        todos.forEach(todo => {
+            printTodo(todo);
         });
-
-        if (!response.ok) {
-            throw new Error('Cannot fetch');
-        }
-
-        const newTodo = await response.json();
+    }
+    
+    function removeTodo(todoId) {
+        todos.filter(todo => todo.id !== todoId);
+    
+        const todo = document.querySelector('ul').querySelector(`[data-id="${todoId}"]`);
+        todo.querySelector('input').removeEventListener('change', handleStatusChange);
+        todo.querySelector('.close').removeEventListener('click', handleClose);
+        todo.remove();
+    }
+    
+    function alertError(error) {
+        alert(error.message);
+    }
+    
+    function createUserOption(user) {
+        const op = document.createElement('option');
+        op.value = user.id;
+        op.innerText = user.name;
         
-        printTodo(newTodo);
-    } catch (error) {
-        alertError(error);
+        return op;
     }
-}
-
-async function changeTodoStatus(todoId, completed) {
-    try {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {
-            method: 'PATCH',
-            body: JSON.stringify({completed}),
-            headers: {
-                'Content-Type': 'application/json',
-            },
+    
+    function printUserOption(user) {
+        const select = document.querySelector('#user-todo');
+        select.append(createUserOption(user));
+    }
+    
+    function printUsersOptions(users) {
+        users.forEach(user => {
+            printUserOption(user);
         });
-
-        if (!response.ok) {
-            throw new Error('Cannot fetch');
-        }
-
-    } catch (error) {
-        alertError(error);
     }
-}
-
-async function deleteTodo(todoId) {
-    try {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+    
+    function handleSubmit(event) {
+        event.preventDefault();
+    
+        createTodo({
+            userId: Number(this.user.value),
+            title: this.todo.value,
+            completed: false,
+        });
+    
+        this.reset();
+    }
+    
+    function handleStatusChange() {
+        changeTodoStatus(this.parentElement.dataset.id, this.checked);
+    }
+    
+    function handleClose() {
+        deleteTodo(this.parentElement.dataset.id);
+    }
+    
+    function initApp() {
+        Promise.all([getAllTodos(), getAllUsers()])
+        .then(values => {
+            todos.push(...values[0]);
+            users.push(...values[1]);
+    
+            printUsersOptions(users);
+            printTodos(todos);
         })
-
-        if (response.ok) {
-            removeTodo(todoId);
-        } else {
-            throw new Error('Cannot fetch');
-        }
-
-    } catch (error) {
-        alertError(error);
+    
+        const form = document.querySelector('form');
+        form.addEventListener('submit', handleSubmit);
     }
-}
+    
+    
+    async function getAllTodos() {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    
+            if (!response.ok) {
+                throw new Error('Cannot fetch');
+            }
+            
+            const data = await response.json();
+        
+            return data   
+        } catch (error) {
+            alertError(error);
+        }
+    }
+    
+    async function getAllUsers() {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    
+            if (!response.ok) {
+                throw new Error('Cannot fetch');
+            }
+    
+            const data = await response.json();
+    
+            return data
+        } catch (error) {
+            alertError(error);
+        }
+    }
+    
+    async function createTodo(todo) {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+                method: 'POST',
+                body: JSON.stringify(todo),
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+    
+            if (!response.ok) {
+                throw new Error('Cannot fetch');
+            }
+    
+            const newTodo = await response.json();
+            
+            printTodo(newTodo);
+        } catch (error) {
+            alertError(error);
+        }
+    }
+    
+    async function changeTodoStatus(todoId, completed) {
+        try {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {
+                method: 'PATCH',
+                body: JSON.stringify({completed}),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+    
+            if (!response.ok) {
+                throw new Error('Cannot fetch');
+            }
+    
+        } catch (error) {
+            alertError(error);
+        }
+    }
+    
+    async function deleteTodo(todoId) {
+        try {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+    
+            if (response.ok) {
+                removeTodo(todoId);
+            } else {
+                throw new Error('Cannot fetch');
+            }
+    
+        } catch (error) {
+            alertError(error);
+        }
+    }
+    
+})();
